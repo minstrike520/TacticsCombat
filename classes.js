810c7c048d7cab1d8c294c6379b801ccdab3345a
minstrike520 <minstrike520@gmail.com>
2023-03-07 21:03:12 +0800

    Uploaded

--- classes.js
+++
@@ -1,4 +0,0 @@
-class Sprite{
-    constructor(){
-    }
-}
--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+import configs
 
 app = Flask(__name__)
+app.config.from_object(configs)
 
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     ctx = render_template("index.html")
-    print(ctx)
     return ctx
 
+@socketio.on('send')
+def chat(data):
+    socketio.emit('get', data)
+
+@socketio.on('test')
+def test():
+    socketio.send("test")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81)
+    socketio.run(app=app, host='0.0.0.0', port=81)
 
 # render_template 只能在flask的裝飾符包裝的函數底下執行。
